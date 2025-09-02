e7c10cbf0b36d7cd054ef28b6c98e6deae74aeac
Yassine El-Alja <yel-alja@c1r1p7.1337.ma>
2025-09-02 10:03:40 +0100

    add: pico shell

---
+++ lvl_1/picoshell/picoshell.c
@@ -0,0 +1,64 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+int pico(char **cmd[])
+{
+    int i = 0;
+    int p = -1;
+    int fd[2];
+    int pid;
+
+    while(cmd[i])
+    {
+        if(cmd[i + 1] && pipe(fd))
+            return 1;
+        pid = fork();
+        if(pid == 0)
+        {
+            if(p != -1)
+            {
+                dup2(p , 0);
+                close(p);
+            }
+            if(cmd[i + 1])
+            {
+                dup2(fd[1] , 1);
+                close(fd[1]);
+            }
+            execvp(cmd[i][0] , cmd[i]);
+            exit(1);
+        }
+        if(p != -1)
+        {
+            close(p);
+            p = -1;
+        }
+        if(cmd[i + 1])
+        {
+            close(fd[1]);
+            p = fd[0];
+        }
+        i++;
+    }
+    i = 0;
+    int status;
+    while(cmd[i])
+    {
+        wait(&status);
+        if(status)
+            return 1;
+        i++;
+    }
+    return (0);
+}
+
+/*
+
+int main()
+{
+    char *ls[] = {"ls" , "-la" , NULL};
+    char *cat[] = {"cat" , "-e" , NULL};
+    char *wc[] = {"wc" , "-l" , NULL};
+    char **arg[] = {ls , cat ,wc, NULL};
+    pico(arg);
+}*/
